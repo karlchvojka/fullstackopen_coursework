a70db3791c3a95df1edf94ff7481d64304a046cc
karlchvojka <karl.chvojka@gmail.com>
2025-09-09 16:27:26 -0400

    Part 1: A more complex state, debugging react: complex state step 1

--- counterdemo/src/App.jsx
+++ counterdemo/src/App.jsx
@@ -1,34 +1,20 @@
 // Framework Imports
 import { useState } from 'react'
 
-const Button = ({ onClick, text}) => <button onClick={onClick}>{text}</button>
-const Display = ({ counter }) => <div>{counter}</div>
-
 const App = () => {
-  // Adds state to the component with the value of 0
-  // Counter variable is assigned value of 0
-  // setCounter is assigned a function that will modify the state
-  const [ counter, setCounter ] = useState(0)
-
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />
+      {left}
+      <button onClick={() => setLeft(left + 1)}>
+        left
+      </button>
+      <button onClick={() => setRight(right + 1)}>
+        right
+      </button>
+      {right}
     </div>
   )
 }
