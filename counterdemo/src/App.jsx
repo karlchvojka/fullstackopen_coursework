46a301828c17a5f8235c48804e724298a5b3a24f
karlchvojka <karl.chvojka@gmail.com>
2025-09-09 14:59:24 -0400

    Part 1: Component state, event handlers: Event handlers
    Updates the app to make it stateful and handle the counter functionality.

--- counterdemo/src/App.jsx
+++ counterdemo/src/App.jsx
@@ -1,5 +1,18 @@
-const App = (props) => {
-  const { counter } = props
+// Framework Imports
+import { useState } from 'react'
+
+const App = () => {
+  // Adds state to the component with the value of 0
+  // Counter variable is assigned value of 0
+  // setCounter is assigned a function that will modify the state
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    // Updates the state by adding 1 to it
+    () => setCounter(counter + 1),
+    1000
+  )
+
   return (
     <div>{counter}</div>
   )
