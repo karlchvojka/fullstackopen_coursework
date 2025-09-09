aec152658bce7c3cd450dd1537c7a762460acb93
karlchvojka <karl.chvojka@gmail.com>
2025-09-09 14:08:47 -0400

    Part 1: Destructuring Streamlined step 1

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,14 +1,13 @@
 const Hello = (props) => {
+  const name = props.name
+  const age = props.age
   // bornYear is known as a "Helper function" because it adds functionality to the component.
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
-  }
+  const bornYear = () => new Date().getFullYear() - age
 
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name}, you are {age} years old
       </p>
       <p>So you were probably born in {bornYear()}</p>
     </div>
