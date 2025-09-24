46d2bf24f92028211de1932bdc082d0dd7e1b5e7
karlchvojka <karl.chvojka@gmail.com>
2025-09-24 17:06:01 -0400

    Part2: Altering data in server: Extracting communication with the backend into a seperate module

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -7,23 +7,34 @@ import { useState, useEffect } from 'react'
 // Component imports
 import Note from './components/Note'
 
+// Services Imports
+import noteService from './services/notes'
+
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
   // On Render
-  const hook = () => {
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        setNotes(response.data)
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+  
+  // Helper Functions
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id === id ? returnedNote : note))
       })
   }
-  
-  useEffect(hook, [])
-
-  // Helper Functions
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
@@ -31,26 +42,16 @@ const App = () => {
       important: Math.random() < 0.5,
     }
     
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        setNotes(notes.concat(response.data))
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
   }
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
-  }
-
-  const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
-
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(note => note.id === id ? response.data : note))
-    })
   }
 
   const notesToShow = showAll
