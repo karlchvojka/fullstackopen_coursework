410aca8f91956df49d664d208b2d6f9c4e6a6629
karlchvojka <karl.chvojka@gmail.com>
2025-09-30 19:34:24 -0400

    Part3: Web and Express

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,7 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const notes = [
+let notes = [
   {
     id: "1",
     content: "HTML is easy",
@@ -18,11 +19,15 @@ const notes = [
   }
 ]
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(notes))
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
